b5fadfe42fc9f5275b593db7db2ecc2d01f75ee8
João Pedro Lança <joaolanca53@gmail.com>
2025-10-06 16:48:17 -0300

    initial: primeiro commit

---
+++ src/test/java/com/SmarDelivery/SmartDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmarDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
