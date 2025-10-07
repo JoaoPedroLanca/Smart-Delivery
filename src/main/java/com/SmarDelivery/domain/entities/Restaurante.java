8fac1a8fce8ccc383edb7af023bfdd460c2bf4fd
João Pedro Lança <joaolanca53@gmail.com>
2025-10-07 17:21:26 -0300

    feat: Adiciona a lista de cardapio e pedidos a abstração da entitade Restaurante

--- src/main/java/com/SmarDelivery/domain/entities/Restaurante.java
+++ src/main/java/com/SmarDelivery/domain/entities/Restaurante.java
@@ -3,15 +3,15 @@ package com.SmarDelivery.domain.entities;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record Restaurante(Long id,
+public record Restaurante(Long restauranteId,
                           String nome,
                           String cnpj,
                           String endereco,
                           String cep,
                           String telefone,
-                          List<String> cardapido,
                           boolean aberto,
                           LocalDateTime horarioAbertura,
                           LocalDateTime horarioFechamento,
-                          Long pedidoId) {
+                          List<Cardapio> cardapido,
+                          List<Pedido> pedidos) {
 }
