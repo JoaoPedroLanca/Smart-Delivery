3416f48b7c9af38f4684692875df35f117b42466
João Pedro Lança <joaolanca53@gmail.com>
2025-10-07 17:04:35 -0300

    feat: Adiciona a Lista de pedidos ao record e muda o tipo de dado da localizacaoAtual para String

--- src/main/java/com/SmarDelivery/domain/entities/Entregador.java
+++ src/main/java/com/SmarDelivery/domain/entities/Entregador.java
@@ -3,16 +3,17 @@ package com.SmarDelivery.domain.entities;
 import com.SmarDelivery.domain.enums.MeioDeTransporte;
 import com.SmarDelivery.domain.enums.Role;
 
+import java.util.List;
 import java.util.Locale;
 
-public record Entregador(Long id,
+public record Entregador(Long entregadorId,
                          String nome,
                          String email,
                          String cpf,
                          String telefone,
                          MeioDeTransporte meioDeTransporte,
                          boolean disponivel,
-                         Locale localizacaoAtual,
+                         String localizacaoAtual,
                          Role role,
-                         Long pedidoId) {
+                         List<Pedido> pedidos) {
 }
