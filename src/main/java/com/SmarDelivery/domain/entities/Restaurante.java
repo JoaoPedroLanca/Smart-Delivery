bc889577f077f85cb7391d33817335c489fd0c83
João Pedro Lança <joaolanca53@gmail.com>
2025-10-07 01:02:58 -0300

    feat: Cria a entidade Restaurante na camada de domain

---
+++ src/main/java/com/SmarDelivery/domain/entities/Restaurante.java
@@ -0,0 +1,16 @@
+package com.SmarDelivery.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record Restaurante(Long id,
+                          String nome,
+                          String cnpj,
+                          String endereco,
+                          String cep,
+                          String telefone,
+                          List<String> cardapido,
+                          boolean aberto,
+                          LocalDateTime horarioAbertura,
+                          LocalDateTime horarioFechamento) {
+}
