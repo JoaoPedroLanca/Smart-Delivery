f049ae147fe6dac0db3ff086c6a9c208446b4c1b
João Pedro Lança <joaolanca53@gmail.com>
2025-10-07 01:08:38 -0300

    feat: Cria a entidade Entregador na camada de domain

---
+++ src/main/java/com/SmarDelivery/domain/entities/Entregador.java
@@ -0,0 +1,17 @@
+package com.SmarDelivery.domain.entities;
+
+import com.SmarDelivery.domain.enums.MeioDeTransporte;
+import com.SmarDelivery.domain.enums.Role;
+
+import java.util.Locale;
+
+public record Entregador(Long id,
+                         String nome,
+                         String email,
+                         String cpf,
+                         String telefone,
+                         MeioDeTransporte meioDeTransporte,
+                         boolean disponivel,
+                         Locale localizacaoAtual,
+                         Role role) {
+}
