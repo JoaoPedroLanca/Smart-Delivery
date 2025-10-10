030ab7b667bfd51c5f47d0c150427b286b02ef35
João Pedro Lança <joaolanca53@gmail.com>
2025-10-10 13:42:43 -0300

    feat: Cria na infra o gateway para implementação do PedidoGateway e sobreescrever os seus métodos

---
+++ src/main/java/com/SmarDelivery/infra/gateway/PedidoRepositoryGateway.java
@@ -0,0 +1,24 @@
+package com.SmarDelivery.infra.gateway;
+
+import com.SmarDelivery.domain.entities.Pedido;
+import com.SmarDelivery.domain.gateway.PedidoGateway;
+import com.SmarDelivery.infra.mappers.PedidoMapper;
+import com.SmarDelivery.infra.persistence.repositories.PedidoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PedidoRepositoryGateway implements PedidoGateway {
+
+    private final PedidoRepository pedidoRepository;
+    private final PedidoMapper pedidoMapper;
+
+    @Override
+    public Pedido criarPedido(Pedido pedido) {
+        var pedidoEntity = pedidoMapper.toEntity(pedido);
+        var pedidoSalvo = pedidoRepository.save(pedidoEntity);
+
+        return pedidoMapper.toDomain(pedidoSalvo);
+    }
+}
