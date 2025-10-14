22654dea81812d2a7a9324d5e791a4271e4553bc
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-14 13:35:00 -0300

    feat: Adiciona no gateway o método para listar todos os clientes

--- src/main/java/com/SmarDelivery/domain/gateway/ClienteGateway.java
+++ src/main/java/com/SmarDelivery/domain/gateway/ClienteGateway.java
@@ -2,11 +2,14 @@ package com.SmarDelivery.domain.gateway;
 
 import com.SmarDelivery.domain.entities.Cliente;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClienteGateway {
 
     Cliente criarCliente(Cliente cliente);
+
+    List<Cliente> buscarTodosOsClientes();
 
     Optional<Cliente> buscarClientePorId(Long clienteId);
 }
