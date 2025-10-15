210a6c5c68d1169b2867b58ddcd66ef007bb48d3
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-15 16:55:13 -0300

    refactor: Refatora o ClienteRequestDto para incluir validations nos atributos

--- src/main/java/com/SmarDelivery/infra/dtos/requests/ClienteRequestDto.java
+++ src/main/java/com/SmarDelivery/infra/dtos/requests/ClienteRequestDto.java
@@ -1,9 +1,15 @@
 package com.SmarDelivery.infra.dtos.requests;
 
-public record ClienteRequestDto(String nome,
-                                String email,
-                                String cpf,
-                                String telefone,
-                                String endereco,
-                                String cep) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record ClienteRequestDto(
+        @NotBlank(message = "O nome não pode ser vazio") String nome,
+        @Email(message = "Email inválido") String email,
+        @CPF(message = "CPF inválido") String cpf,
+        @Pattern(regexp = "\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}", message = "Telefone inválido") String telefone,
+        @NotBlank(message = "O endereço não pode ser vazio") String endereco,
+        @Pattern(regexp = "\\d{5}-?\\d{3}", message = "CEP inválido") String cep) {
 }
