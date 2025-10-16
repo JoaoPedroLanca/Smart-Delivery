4a5fc8f1a9a394d38892abd2584df28bf61f4628
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-16 15:50:05 -0300

    feat: Cria o RestauranteGateway no domain para comunicação entre domain e infra

---
+++ src/main/java/com/SmarDelivery/domain/gateway/RestauranteGateway.java
@@ -0,0 +1,8 @@
+package com.SmarDelivery.domain.gateway;
+
+import com.SmarDelivery.domain.entities.Restaurante;
+
+public interface RestauranteGateway {
+
+    Restaurante criarRestaurante(Restaurante restaurante);
+}
