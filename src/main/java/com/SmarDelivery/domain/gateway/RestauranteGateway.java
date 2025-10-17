672f76739fb851fa8b58a103a30a9136dfa370d5
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-17 15:21:23 -0300

    feat: Adiciona ao RestauranteGateway os métodos de listar todos os restaurantes e buscar por id

--- src/main/java/com/SmarDelivery/domain/gateway/RestauranteGateway.java
+++ src/main/java/com/SmarDelivery/domain/gateway/RestauranteGateway.java
@@ -2,7 +2,14 @@ package com.SmarDelivery.domain.gateway;
 
 import com.SmarDelivery.domain.entities.Restaurante;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RestauranteGateway {
+
+    List<Restaurante> buscarTodosRestaurantes();
+
+    Optional<Restaurante> buscarRestaurantePorId(Long restauranteId);
 
     Restaurante criarRestaurante(Restaurante restaurante);
 }
