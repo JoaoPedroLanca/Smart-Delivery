5902296d3a8ef09d6b27f648058f620259b36fcd
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-17 15:44:23 -0300

    feat: Adiciona o endpoint de buscar todos os restaurantes e o endpoint de buscar por id

--- src/main/java/com/SmarDelivery/infra/presentation/RestauranteController.java
+++ src/main/java/com/SmarDelivery/infra/presentation/RestauranteController.java
@@ -1,16 +1,19 @@
 package com.SmarDelivery.infra.presentation;
 
+import com.SmarDelivery.domain.entities.Restaurante;
+import com.SmarDelivery.domain.usecases.restaurante.BuscarRestaurantePorIdUsecase;
+import com.SmarDelivery.domain.usecases.restaurante.BuscarTodosRestaurantesUsecase;
 import com.SmarDelivery.domain.usecases.restaurante.CriarRestauranteUsecase;
 import com.SmarDelivery.infra.dtos.requests.restaurante.RestauranteRequestDto;
 import com.SmarDelivery.infra.dtos.responses.restaurante.RestauranteResponseDto;
 import com.SmarDelivery.infra.mappers.RestauranteMapper;
+import com.SmarDelivery.infra.persistence.entities.RestauranteEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestauranteController {
 
     private final CriarRestauranteUsecase criarRestauranteUsecase;
+    private final BuscarTodosRestaurantesUsecase buscarTodosRestaurantesUsecase;
+    private final BuscarRestaurantePorIdUsecase buscarRestaurantePorIdUsecase;
     private final RestauranteMapper restauranteMapper;
+
+    @GetMapping
+    public ResponseEntity<List<RestauranteResponseDto>> buscarTodosRestaurantes() {
+        List<Restaurante> restaurantes = buscarTodosRestaurantesUsecase.execute();
+        List<RestauranteResponseDto> restauranteResponse = restaurantes
+                .stream()
+                .map(restauranteMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(restauranteResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestauranteResponseDto> buscarRestaurantePorId(@PathVariable Long id) {
+        Restaurante restaurante = buscarRestaurantePorIdUsecase.execute(id);
+        RestauranteResponseDto response = restauranteMapper.toResponse(restaurante);
+
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping
     public ResponseEntity<RestauranteResponseDto> criarRestaurante(@RequestBody RestauranteRequestDto requestDto) {
