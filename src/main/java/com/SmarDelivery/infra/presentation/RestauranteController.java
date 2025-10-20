11bd6e0ada25c1f15eb37b5480929b7516d292e8
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-19 23:59:43 -0300

    feat: Adiciona o endpoint de Patch para atualizar os dados do Restaurante

--- src/main/java/com/SmarDelivery/infra/presentation/RestauranteController.java
+++ src/main/java/com/SmarDelivery/infra/presentation/RestauranteController.java
@@ -1,13 +1,14 @@
 package com.SmarDelivery.infra.presentation;
 
 import com.SmarDelivery.domain.entities.Restaurante;
+import com.SmarDelivery.domain.usecases.restaurante.AtualizarRestauranteUsecase;
 import com.SmarDelivery.domain.usecases.restaurante.BuscarRestaurantePorIdUsecase;
 import com.SmarDelivery.domain.usecases.restaurante.BuscarTodosRestaurantesUsecase;
 import com.SmarDelivery.domain.usecases.restaurante.CriarRestauranteUsecase;
+import com.SmarDelivery.infra.dtos.requests.restaurante.PatchRestauranteRequestDto;
 import com.SmarDelivery.infra.dtos.requests.restaurante.RestauranteRequestDto;
 import com.SmarDelivery.infra.dtos.responses.restaurante.RestauranteResponseDto;
 import com.SmarDelivery.infra.mappers.RestauranteMapper;
-import com.SmarDelivery.infra.persistence.entities.RestauranteEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ public class RestauranteController {
     private final CriarRestauranteUsecase criarRestauranteUsecase;
     private final BuscarTodosRestaurantesUsecase buscarTodosRestaurantesUsecase;
     private final BuscarRestaurantePorIdUsecase buscarRestaurantePorIdUsecase;
+    private final AtualizarRestauranteUsecase atualizarRestauranteUsecase;
     private final RestauranteMapper restauranteMapper;
 
     @GetMapping
@@ -50,5 +52,15 @@ public class RestauranteController {
         var restauranteResponse = restauranteMapper.toResponse(criarRestauranteUsecase.execute(restauranteDomain));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(restauranteResponse);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<RestauranteResponseDto> atualizarRestaurante(@PathVariable Long id, @RequestBody PatchRestauranteRequestDto patchDto) {
+        Restaurante restauranteExiste = buscarRestaurantePorIdUsecase.execute(id);
+        Restaurante restauranteAtualizado = restauranteMapper.patchToRestaurante(patchDto, restauranteExiste);
+        restauranteAtualizado = atualizarRestauranteUsecase.execute(restauranteAtualizado);
+        RestauranteResponseDto responseDto = restauranteMapper.toResponse(restauranteAtualizado);
+
+        return ResponseEntity.ok(responseDto);
     }
 }
