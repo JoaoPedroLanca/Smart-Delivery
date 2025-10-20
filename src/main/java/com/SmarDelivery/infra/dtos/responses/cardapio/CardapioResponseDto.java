2fd4b6d9d035463e8828a0072f8f98566a505490
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-20 14:32:01 -0300

    fix: Remove a redundancia de mostrar o restauranteId no Json de resposta

--- src/main/java/com/SmarDelivery/infra/dtos/responses/cardapio/CardapioResponseDto.java
+++ src/main/java/com/SmarDelivery/infra/dtos/responses/cardapio/CardapioResponseDto.java
@@ -4,6 +4,5 @@ public record CardapioResponseDto(
         Long produtoId,
         String nome,
         double preco,
-        boolean disponivel,
-        Long restauranteId) {
+        boolean disponivel) {
 }
