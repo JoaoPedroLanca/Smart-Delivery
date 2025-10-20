e875e92fc9919572723eb2e0a791782127f820fa
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-20 14:33:21 -0300

    refactor: Refatora os mapeamentos do CardapioMapper para a forma correta

--- src/main/java/com/SmarDelivery/infra/mappers/CardapioMapper.java
+++ src/main/java/com/SmarDelivery/infra/mappers/CardapioMapper.java
@@ -1,7 +1,6 @@
 package com.SmarDelivery.infra.mappers;
 
 import com.SmarDelivery.domain.entities.Cardapio;
-import com.SmarDelivery.domain.entities.Cliente;
 import com.SmarDelivery.infra.dtos.requests.cardapio.CardapioRequestDto;
 import com.SmarDelivery.infra.dtos.requests.cardapio.PatchCardapioRequestDto;
 import com.SmarDelivery.infra.dtos.responses.cardapio.CardapioResponseDto;
@@ -11,24 +10,28 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface CardapioMapper {
 
+    @Mapping(target = "produtoId", ignore = true)
+    @Mapping(target = "restaurante_id", source = "restauranteId")
     Cardapio toDomain(CardapioRequestDto dto);
 
+    @Mapping(target = "restaurante", ignore = true)
     CardapioEntity toEntity(Cardapio domain);
 
+    @Mapping(target = "restaurante_id", source = "restaurante.restauranteId")
     Cardapio toDomain(CardapioEntity entity);
 
     CardapioResponseDto toResponse(Cardapio domain);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Cardapio updateCardapioFromDto(PatchCardapioRequestDto dto, @MappingTarget Cliente cliente);
+    Cardapio updateCardapioFromDto(PatchCardapioRequestDto dto, @MappingTarget Cardapio cardapio);
 
     default Cardapio patchToCardapio(PatchCardapioRequestDto dto, Cardapio original) {
         return new Cardapio(
-            original.produtoId(),
-            dto.nome() != null ? dto.nome() : original.nome(),
-            original.preco(),
-            original.disponivel(),
-            original.restaurante_id()
+                original.produtoId(),
+                dto.nome() != null ? dto.nome() : original.nome(),
+                dto.preco() != 0.0 ? dto.preco() : original.preco(),
+                dto.disponivel(),
+                dto.restauranteId() != null ? dto.restauranteId() : original.restaurante_id()
         );
     }
 }
