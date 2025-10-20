de1a12a849dd8ba4b4995d8d6fd065bbfbe0acfc
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-20 14:29:25 -0300

    feat: Cria o Gateway do cardápio na camada de infra para sobreescrever os métodos

---
+++ src/main/java/com/SmarDelivery/infra/gateway/CardapioRepositoryGateway.java
@@ -0,0 +1,31 @@
+package com.SmarDelivery.infra.gateway;
+
+import com.SmarDelivery.domain.entities.Cardapio;
+import com.SmarDelivery.domain.gateway.CardapioGateway;
+import com.SmarDelivery.infra.mappers.CardapioMapper;
+import com.SmarDelivery.infra.persistence.entities.CardapioEntity;
+import com.SmarDelivery.infra.persistence.entities.RestauranteEntity;
+import com.SmarDelivery.infra.persistence.repositories.CardapioRepository;
+import com.SmarDelivery.infra.persistence.repositories.RestauranteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CardapioRepositoryGateway implements CardapioGateway {
+
+    private final CardapioRepository cardapioRepository;
+    private final CardapioMapper cardapioMapper;
+    private final RestauranteRepository restauranteRepository;
+
+    @Override
+    public Cardapio criarCardapio(Cardapio cardapio) {
+        var cardapioEntity = cardapioMapper.toEntity(cardapio);
+        var restauranteId = cardapio.restaurante_id();
+        var restauranteEntity = restauranteRepository.findById(restauranteId)
+                .orElseThrow(() -> new RuntimeException("Restaurante não encontrado"));
+        cardapioEntity.setRestaurante(restauranteEntity);
+        var novoCardapio = cardapioRepository.save(cardapioEntity);
+        return cardapioMapper.toDomain(novoCardapio);
+    }
+}
