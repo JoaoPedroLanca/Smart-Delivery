13522809bd91026875f7c9a564e145f33f5ac4d0
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-20 14:51:14 -0300

    feat: Adiciona o método para buscar o cardápio por id no CardapioGateway

--- src/main/java/com/SmarDelivery/domain/gateway/CardapioGateway.java
+++ src/main/java/com/SmarDelivery/domain/gateway/CardapioGateway.java
@@ -2,7 +2,11 @@ package com.SmarDelivery.domain.gateway;
 
 import com.SmarDelivery.domain.entities.Cardapio;
 
+import java.util.Optional;
+
 public interface CardapioGateway {
 
     Cardapio criarCardapio(Cardapio cardapio);
+
+    Optional<Cardapio> buscarCardapioPorId(Long cardapioId);
 }
