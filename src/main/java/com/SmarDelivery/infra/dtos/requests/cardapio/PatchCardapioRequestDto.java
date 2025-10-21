5974b9a155f6b2fd45bd324695daaffa5991ff6a
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-21 13:07:13 -0300

    refactor: Altera a tipagem dos dados para não primitivos

--- src/main/java/com/SmarDelivery/infra/dtos/requests/cardapio/PatchCardapioRequestDto.java
+++ src/main/java/com/SmarDelivery/infra/dtos/requests/cardapio/PatchCardapioRequestDto.java
@@ -1,13 +1,8 @@
 package com.SmarDelivery.infra.dtos.requests.cardapio;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
 public record PatchCardapioRequestDto(
         String nome,
-        double preco,
-        boolean disponivel,
-        Long restauranteId
+        Double preco,
+        Boolean disponivel
 ) {
 }
