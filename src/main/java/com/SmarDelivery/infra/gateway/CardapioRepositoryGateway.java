23e52dbcd9699778ce1497a73b88a8104d7351b7
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-22 14:26:45 -0300

    feat: Adiciona método para atualizar o cardápio por meio de Patch

--- src/main/java/com/SmarDelivery/infra/gateway/CardapioRepositoryGateway.java
+++ src/main/java/com/SmarDelivery/infra/gateway/CardapioRepositoryGateway.java
@@ -2,14 +2,18 @@ package com.SmarDelivery.infra.gateway;
 
 import com.SmarDelivery.domain.entities.Cardapio;
 import com.SmarDelivery.domain.gateway.CardapioGateway;
+import com.SmarDelivery.infra.dtos.requests.cardapio.PatchCardapioRequestDto;
 import com.SmarDelivery.infra.mappers.CardapioMapper;
 import com.SmarDelivery.infra.persistence.entities.CardapioEntity;
-import com.SmarDelivery.infra.persistence.entities.RestauranteEntity;
 import com.SmarDelivery.infra.persistence.repositories.CardapioRepository;
-import com.SmarDelivery.infra.persistence.repositories.RestauranteRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -18,16 +22,11 @@ public class CardapioRepositoryGateway implements CardapioGateway {
 
     private final CardapioRepository cardapioRepository;
     private final CardapioMapper cardapioMapper;
-    private final RestauranteRepository restauranteRepository;
 
     @Override
     public Cardapio criarCardapio(Cardapio cardapio) {
-        var cardapioEntity = cardapioMapper.toEntity(cardapio);
-        var restauranteId = cardapio.restaurante_id();
-        var restauranteEntity = restauranteRepository.findById(restauranteId)
-                .orElseThrow(() -> new RuntimeException("Restaurante não encontrado"));
-        cardapioEntity.setRestaurante(restauranteEntity);
-        var novoCardapio = cardapioRepository.save(cardapioEntity);
+        CardapioEntity cardapioEntity = cardapioMapper.toEntity(cardapio);
+        CardapioEntity novoCardapio = cardapioRepository.save(cardapioEntity);
         return cardapioMapper.toDomain(novoCardapio);
     }
 
@@ -35,5 +34,23 @@ public class CardapioRepositoryGateway implements CardapioGateway {
     public Optional<Cardapio> buscarCardapioPorId(Long cardapioId) {
         Optional<CardapioEntity> cardapio = cardapioRepository.findById(cardapioId);
         return cardapio.map(cardapioMapper::toDomain);
+    }
+
+    @Transactional
+    @Override
+    public Cardapio atualizarCardapio(Long cardapioId, Map<String, Object> atualizacao) {
+        CardapioEntity cardapioEntity = cardapioRepository.findById(cardapioId)
+                .orElseThrow(() -> new RuntimeException("Cardápio" + cardapioId + " não encontrado em sistema para atualização"));
+        Cardapio cardapioDomain = cardapioMapper.toDomain(cardapioEntity);
+        ObjectMapper objectMapper = new ObjectMapper();
+        PatchCardapioRequestDto patchDto = objectMapper.convertValue(atualizacao, PatchCardapioRequestDto.class);
+        Cardapio cardapioAtualizado = merge(patchDto, cardapioDomain);
+        CardapioEntity cardapioAtualizadoEntity = cardapioMapper.toEntity(cardapioAtualizado);
+        CardapioEntity novoCardapio = cardapioRepository.save(cardapioAtualizadoEntity);
+        return cardapioMapper.toDomain(novoCardapio);
+    }
+
+    private Cardapio merge(PatchCardapioRequestDto patchDto, Cardapio cardapio) {
+        return cardapioMapper.patchToCardapio(patchDto, cardapio);
     }
 }
