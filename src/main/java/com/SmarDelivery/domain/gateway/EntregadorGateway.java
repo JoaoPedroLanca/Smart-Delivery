571e795d8700745114501acb16efa8a97839b3f7
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-28 17:16:26 -0300

    feat: Cria método para patch dos dados do entregador

--- src/main/java/com/SmarDelivery/domain/gateway/EntregadorGateway.java
+++ src/main/java/com/SmarDelivery/domain/gateway/EntregadorGateway.java
@@ -3,6 +3,7 @@ package com.SmarDelivery.domain.gateway;
 import com.SmarDelivery.domain.entities.Entregador;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface EntregadorGateway {
@@ -12,4 +13,6 @@ public interface EntregadorGateway {
     List<Entregador> buscarTodosEntregadores();
 
     Optional<Entregador> buscarEntregadorPorId(Long entregadorId);
+
+    Entregador atualizarEntregador(Long entregadorId, Map<String, Object> atualizacao);
 }
