d612764267c2b52a7eeb28e1cecc62649998ddfa
JoaoPedroLanca <joaolanca53@gmail.com>
2025-10-28 17:17:49 -0300

    Sobreescreve o método de de patch na camada de infra

--- src/main/java/com/SmarDelivery/infra/gateway/EntregadorRepositoryGateway.java
+++ src/main/java/com/SmarDelivery/infra/gateway/EntregadorRepositoryGateway.java
@@ -2,12 +2,16 @@ package com.SmarDelivery.infra.gateway;
 
 import com.SmarDelivery.domain.entities.Entregador;
 import com.SmarDelivery.domain.gateway.EntregadorGateway;
+import com.SmarDelivery.infra.dtos.requests.entregador.PatchEntregadorRequestDto;
 import com.SmarDelivery.infra.mappers.EntregadorMapper;
+import com.SmarDelivery.infra.persistence.entities.EntregadorEntity;
 import com.SmarDelivery.infra.persistence.repositories.EntregadorRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -25,6 +29,12 @@ public class EntregadorRepositoryGateway implements EntregadorGateway {
     }
 
     @Override
+    public Optional<Entregador> buscarEntregadorPorId(Long entregadorId) {
+        var entregador = entregadorRepository.findById(entregadorId);
+        return entregador.map(entregadorMapper::toDomain);
+    }
+
+    @Override
     public List<Entregador> buscarTodosEntregadores() {
         return entregadorRepository.findAll()
                 .stream()
@@ -33,8 +43,19 @@ public class EntregadorRepositoryGateway implements EntregadorGateway {
     }
 
     @Override
-    public Optional<Entregador> buscarEntregadorPorId(Long entregadorId) {
-        var entregador = entregadorRepository.findById(entregadorId);
-        return entregador.map(entregadorMapper::toDomain);
+    public Entregador atualizarEntregador(Long entregadorId, Map<String, Object> atualizacao) {
+        EntregadorEntity entregadorEntity = entregadorRepository.findById(entregadorId).orElseThrow(() -> new RuntimeException("Entregador não encontrado em sistema para atualização"));
+        Entregador entregadorDomain = entregadorMapper.toDomain(entregadorEntity);
+        ObjectMapper objectMapper = new ObjectMapper();
+        var patchDto = objectMapper.convertValue(atualizacao, PatchEntregadorRequestDto.class);
+        Entregador entregadorAtualizado = merge(patchDto, entregadorDomain);
+        EntregadorEntity entregadorAtualizadoEntity = entregadorMapper.toEntity(entregadorAtualizado);
+        EntregadorEntity novoEntregador = entregadorRepository.save(entregadorAtualizadoEntity);
+
+        return entregadorMapper.toDomain(novoEntregador);
+    }
+
+    private Entregador merge(PatchEntregadorRequestDto patchDto, Entregador entregador) {
+        return entregadorMapper.patchToEntregador(patchDto, entregador);
     }
 }
