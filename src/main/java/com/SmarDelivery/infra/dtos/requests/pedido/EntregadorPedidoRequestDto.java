2b7ba70efc6bde5df9ab446ce7aacff0696c3c80
JoaoPedroLanca <joaolanca53@gmail.com>
2025-12-04 11:32:36 -0300

    refactor: Altera o nome do Dto para melhor entendimento

--- src/main/java/com/SmarDelivery/infra/dtos/requests/pedido/EntregadorPedidoRequestDto.java
+++ src/main/java/com/SmarDelivery/infra/dtos/requests/pedido/EntregadorPedidoRequestDto.java
@@ -2,7 +2,7 @@ package com.SmarDelivery.infra.dtos.requests.pedido;
 
 import jakarta.validation.constraints.NotNull;
 
-public record AceitarPedidoRequestDto(
+public record EntregadorPedidoRequestDto(
         @NotNull(message = "O ID do entregador é obrigatório")
         Long entregadorId) {
 }
