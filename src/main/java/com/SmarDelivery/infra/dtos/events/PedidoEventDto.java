cd10a63b182a2d575534c2f37e69ce9f997ab4ac
JoaoPedroLanca <joaolanca53@gmail.com>
2025-12-05 11:03:51 -0300

    fix: Corrige erro de tipagem do timestampEvento

--- src/main/java/com/SmarDelivery/infra/dtos/events/PedidoEventDto.java
+++ src/main/java/com/SmarDelivery/infra/dtos/events/PedidoEventDto.java
@@ -2,6 +2,8 @@ package com.SmarDelivery.infra.dtos.events;
 
 import com.SmarDelivery.domain.enums.StatusDoPedido;
 
+import java.time.LocalDateTime;
+
 public record PedidoEventDto(
         Long pedidoId,
         Long clienteId,
@@ -9,6 +11,6 @@ public record PedidoEventDto(
         Long entregadorId,
         StatusDoPedido statusDoPedido,
         String tipoEvento,
-        String timestampEvento
+        LocalDateTime timestampEvento
 ) {
 }
